db953d0a9f82e1e79b22a92352c0143f368a178f
Yessine Ben Hlima <yessine.benhlima@cofat.local>
2022-08-23 13:39:54 +0100

    V1.1

--- src/App.js
+++ src/App.js
@@ -20,40 +20,46 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <button onClick={this.handleClick}>
-          {this.state.show ? "Hidden" : "Show"}
-        </button>
-        {this.state.show ? (
-          <div className="card">
-            <img
-              src={this.state.Person.imgSrc}
-              width="120px"
-              height="120px"
-              alt=""
-            />
-            <div className="person">
-              <label>Full Name:</label>
-              <input type="text" value={this.state.Person.fullName} readOnly />
-
-              <label>Age:</label>
-              <input
-                type="text"
-                value={this.state.Person.age + " years old"}
-                readOnly
+        <div className="container">
+          <button onClick={this.handleClick}>
+            {this.state.show ? "Hidden" : "Show"}
+          </button>
+          {this.state.show ? (
+            <div className="card">
+              <img
+                src={this.state.Person.imgSrc}
+                width="120px"
+                height="120px"
+                alt=""
               />
+              <div className="person">
+                <label>Full Name:</label>
+                <input
+                  type="text"
+                  value={this.state.Person.fullName}
+                  readOnly
+                />
 
-              <label>Profession:</label>
-              <input
-                type="text"
-                value={this.state.Person.profession}
-                readOnly
-              />
+                <label>Age:</label>
+                <input
+                  type="text"
+                  value={this.state.Person.age + " years old"}
+                  readOnly
+                />
+
+                <label>Profession:</label>
+                <input
+                  type="text"
+                  value={this.state.Person.profession}
+                  readOnly
+                />
+              </div>
+              <Time />
             </div>
-            <Time />
-          </div>
-        ) : (
-          <h2>Click to show the profile</h2>
-        )}
+          ) : (
+            <h2>Click to show the profile</h2>
+          )}
+        </div>
       </div>
     );
   }
